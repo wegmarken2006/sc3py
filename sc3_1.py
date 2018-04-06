929ec9231ee05da2ba99bf57b818986145a8a0d7
Gigi <wegmarken2006@libero.it>
2018-04-06 20:54:51 +0200

    mce_transform tested

--- sc3_1.py
+++ sc3_1.py
@@ -143,4 +143,68 @@ def rate_of(ugen: Ugen) -> Rate:
     else:
         return Rate.RateKr
     
-
+def mce_degree(ugen: Ugen) -> int:
+    if isinstance(ugen, Mce):
+        ugen = cast(Mce, ugen)
+        return len(ugen.ugens)
+    elif isinstance(ugen, Mrg):
+        ugen = cast(Mrg, ugen)
+        return mce_degree(ugen.left)
+    else:
+        raise Exception("mce_degree")
+ 
+def mce_extend(n, ugen: Ugen) -> List[Ugen]:
+    if isinstance(ugen, Mce):
+        ugen = cast(Mce, ugen)
+        return extend(ugen.ugens, n)
+    elif isinstance(ugen, Mrg):
+        ugen = cast(Mrg, ugen)
+        ex = mce_extend(n, ugen.left)
+        if len(ex) > 0:
+            out: List[Ugen] = []
+            out = [ugen] + ex[1:]
+            return out
+        else:
+            raise Exception("mce_extend")    
+    else:
+        out: List[Ugen] = []
+        for ind in range(1, n+1):
+            out = out + [ugen]
+        return out
+ 
+def is_mce(ugen:Ugen) -> bool:
+    if isinstance(ugen, Mce):
+        return True
+    else:
+        return False
+    
+def transposer(ugens: List[List[Ugen]]) -> List[List[Ugen]]:
+    len1 = len(ugens)
+    len2 = len(ugens[0])
+    out: List[List[Ugen]] = []
+    for ind in range(0, len2):
+        out.append([])
+    for ind2 in range(0, len2):
+        for ind1 in range(0, len1):
+            out[ind2].append(ugens[ind1][ind2])
+    return out
+        
+def mce_transform(ugen: Ugen) -> Ugen:
+    if isinstance(ugen, Primitive):
+        ins = filter(is_mce, ugen.inputs)
+        degs = []
+        for elem in ins:
+            degs = degs + [mce_degree(elem)]
+        upr = max_num(degs, 0)
+        ext: List[List[Ugen]] = []
+        for elem in ugen.inputs:
+            ext.append(mce_extend(upr, elem))
+        iet = transposer(ext)
+        out: List[Ugen] = []
+        p = ugen
+        for elem in iet:
+            p.inputs = elem
+            out.append(p)
+        return Mce(ugens=out)
+    else:
+        raise Exception("mce_transform")
--- test1.py
+++ test1.py
@@ -8,12 +8,12 @@ Created on Fri Apr  6 18:31:52 2018
 import unittest
 from typing import List
 from sc3_1 import Ugen, Constant, Control, Rate, Primitive, Mce, Mrg
-from sc3_1 import extend, rate_of
+from sc3_1 import extend, rate_of, mce_degree, mce_extend, mce_transform
 
 ugens1: List[Ugen] = []
 ugens1.append(Constant(value=1))
 ugens1.append(Constant(value=3.3))
-p1 = Primitive(rate=Rate.RateKr, name="P1", inputs=ugens1,	
+p1 = Primitive(rate=Rate.RateKr, name="P1", inputs=[ugens1, ugens1],	
                outputs=[Rate.RateKr, Rate.RateIr], index=0, special=0)
 p2 = Primitive(rate=Rate.RateAr, name="P2")
 mc1 = Mce(ugens=[p1, p1])
@@ -28,6 +28,8 @@ mg2 = Mrg(left = p2, right = p1)
 mg3 = Mrg(left=mc1, right=p2)
 ugens2 = extend(p1.inputs, 5)
 ct1 = Control(rate=Rate.RateAr, name="Ct1")
+exmg1 = mce_extend(3, mg1)
+mc10 = mce_transform(p3)
 
 class TestStringMethods(unittest.TestCase):
     
@@ -38,7 +40,11 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(type(p3), Primitive)
         self.assertEqual(len(ugens2), 5)
         self.assertEqual(rate_of(ct1), Rate.RateAr)
-
+        self.assertEqual(mce_degree(mc1), 2)
+        self.assertEqual(len(exmg1), 3)
+        self.assertEqual(type(mc10), Mce)
+        self.assertEqual(type(mc10.ugens[2]), Primitive)
+        self.assertEqual(mc10.ugens[2].name, "P3")
 
 if __name__ == '__main__':
     unittest.main()
